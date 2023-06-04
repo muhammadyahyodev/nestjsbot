82856aecccf30430b58a8df49b9bc054f79daadc
muhammadyahyo <muhammadyahyodev@gmail.com>
2023-06-05 00:45:26 +0500

    Update: service 'Start' command

--- src/app.update.ts
+++ src/app.update.ts
@@ -1,12 +1,14 @@
-import { Ctx, Start } from 'nestjs-telegraf';
+import { Ctx, Start, Update } from 'nestjs-telegraf';
 import { Context } from 'telegraf';
 import { AppService } from './app.service';
 
+@Update()
 export class AppUpdate {
   constructor(private readonly appService: AppService) {}
 
   @Start()
   async onStart(@Ctx() ctx: Context) {
+    console.log('CONTEXT: ', Context);
     return this.appService.onStart(ctx);
   }
 }
